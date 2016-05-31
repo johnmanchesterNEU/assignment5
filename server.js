3c1245a9b475d78f7d3023e0dba7a87827e63f34
johnmanchesterNEU <manchester.j@husky.neu.edu>
2016-05-31 00:00:12 -0400

    try again

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 //  OpenShift sample Node application
 var express = require('express');
 var fs      = require('fs');
-
-
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 /**
  *  Define the sample application.
  */
